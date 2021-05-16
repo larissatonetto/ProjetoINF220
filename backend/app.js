3dff92d5d787b3772284d3c0cb93379bb4f1ab33
lucaskermessi <lucaskermessi@gmail.com>
2021-05-15 21:40:17 -0300

    clienteCadastrado

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,13 @@ const port = process.env.PORT || 5000
 app.use (express.urlencoded({extended: false}))
 app.use(express.json()) 
 
-//MariaDB
-const connection = mariadb.createConnection({
+const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'elComprador'
-})
+    database: 'elComprador',
+    connectionLimit: 5
+});
 
 
 //HandleBars
@@ -27,8 +27,21 @@ app.get('/registro-cliente', (req,res) =>{
     res.render('cliente')
 })
 
-app.post('/addCliente', (req,res) =>{
-    res.send('Nome: '+req.body.nome)
+//CADASTRO DE CLIENTE
+app.post('/addCliente', async(req,res) =>{
+    const conn = await pool.getConnection();
+    const {nome,sobrenome,celular, email, senha} = req.body
+    const {cidade, numero, bairro, rua, complemento, geolocalizacao, UF, apelido} = req.body
+    const {tipo, numCartao, nomeTitular, validade, CVV} = req.body
+
+    let result = await conn.query("INSERT INTO `USUARIO` (`nome`, `sobrenome`, `celular`, `email`, `senha`) VALUES (?, ?, ?,?,?);", [nome, sobrenome, celular,email,senha]);
+    const idUser = result.insertId
+    result = await conn.query("INSERT INTO `CLIENTE` (`idUser`) VALUES (?);", [idUser]);
+    const idCliente = result.insertId
+    await conn.query("INSERT INTO `ENDERECO`(`idCliente`, `cidade`, `numero`, `bairro`, `rua`, `complemento`, `geolocalizacao`, `UF`, `apelido`) VALUES (?,?,?,?,?,?,?,?,?) ", [idCliente, cidade, numero, bairro, rua, complemento, geolocalizacao, UF, apelido])
+    result = await conn.query("INSERT INTO `METODOPAGAMENTO` (`idCliente`) VALUES (?);", [idCliente]);
+    const idPagamento = result.insertId
+    await conn.query("INSERT INTO `CARTAO` (`idMetodoPagamento`, `tipo`, `numero`, `nomeTitular`, `validade`, `CVV`) VALUES (?, ?, ?, ?, ?, ?);", [idPagamento, tipo, numCartao, nomeTitular, validade, CVV]);
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
